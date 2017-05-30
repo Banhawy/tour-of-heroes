f8afcce5aec86a95c6bd2d4d8268b24e01b5967e
Adham El Banhawy <the.benhawy@gmail.com>
2017-05-29 23:10:04 -0500

    Imported router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -9,7 +10,13 @@ import { HeroService } from './hero.service';
 
 @NgModule({
   imports:      [ BrowserModule, 
-                  FormsModule 
+                  FormsModule,
+                  RouterModule.forRoot([
+                    {
+                      path: 'heroes',
+                      component: HeroesComponent
+                    }
+                  ])
                   ],
   declarations: [ AppComponent,
                  HeroDetailComponent,
@@ -18,4 +25,6 @@ import { HeroService } from './hero.service';
   bootstrap:    [ AppComponent ],
   providers: [HeroService]
 })
+
+
 export class AppModule { }
