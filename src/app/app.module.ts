49e464caa4e862d70a3efca04d8ba60f63b6670d
Adham El Banhawy <the.benhawy@gmail.com>
2017-05-29 20:39:08 -0500

    Split up the AppComponent and created HeroesComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { FormsModule } from '@angular/forms'
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports:      [ BrowserModule, 
                   FormsModule 
                   ],
   declarations: [ AppComponent,
-                 HeroDetailComponent 
+                 HeroDetailComponent,
+                 HeroesComponent
                  ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [HeroService]
 })
 export class AppModule { }
